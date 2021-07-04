6508b094c05c200a49917445f149db09cc78dbad
roman <rhromets@gmail.com>
2021-07-04 22:29:15 +0300

    xylophone functionality

--- lib/main.dart
+++ lib/main.dart
@@ -4,19 +4,38 @@ import 'package:audiofileplayer/audiofileplayer.dart';
 void main() => runApp(XylophoneApp());
 
 class XylophoneApp extends StatelessWidget {
+  void playSound(int noteNumber) {
+    Audio.load('assets/note$noteNumber.wav')..play();
+  }
+
+  Expanded buildKey({Color color = Colors.black, int noteNumber = 1}) {
+    return Expanded(
+      child: TextButton(
+        style: TextButton.styleFrom(backgroundColor: color),
+        onPressed: () {
+          playSound(noteNumber);
+        },
+        child: Text(""),
+      ),
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       home: Scaffold(
+        backgroundColor: Colors.black,
         body: SafeArea(
-          child: Center(
-            child: TextButton(
-              onPressed: () {
-                Audio.load('assets/note1.wav')..play();
-              },
-              child: Text("Click me"),
-            ),
-          ),
+          child:
+              Column(crossAxisAlignment: CrossAxisAlignment.stretch, children: [
+            buildKey(color: Colors.red, noteNumber: 1),
+            buildKey(color: Colors.orange, noteNumber: 2),
+            buildKey(color: Colors.yellow, noteNumber: 3),
+            buildKey(color: Colors.green, noteNumber: 4),
+            buildKey(color: Colors.teal, noteNumber: 5),
+            buildKey(color: Colors.blue, noteNumber: 6),
+            buildKey(color: Colors.purple, noteNumber: 7),
+          ]),
         ),
       ),
     );
